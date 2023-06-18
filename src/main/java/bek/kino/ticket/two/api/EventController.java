184247530bca9e3d201e70f2afef52c472b0735a
Bexultan Kuralbay <30636@iitu.edu.kz>
2023-06-18 13:00:07 +0600

    rest controller

---
+++ src/main/java/bek/kino/ticket/two/api/EventController.java
@@ -0,0 +1,10 @@
+package bek.kino.ticket.two.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class EventController {
+}
---
+++ src/main/java/bek/kino/ticket/two/api/HallController.java
@@ -0,0 +1,11 @@
+package bek.kino.ticket.two.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class HallController {
+}
