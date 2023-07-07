2033307978b9e87f674df987dbc6f447304bf455
Bexultan Kuralbay <30636@iitu.edu.kz>
2023-07-07 23:22:22 +0600

    refactoring

--- src/main/resources/db/changelog/changes/create_init_tables.sql
+++ src/main/resources/db/changelog/changes/create_init_tables.sql
@@ -1,55 +1,62 @@
-CREATE TABLE halls (
-    id SERIAL PRIMARY KEY ,
+CREATE TABLE halls
+(
+    id    SERIAL PRIMARY KEY,
     place INT
 );
-CREATE TABLE t_permissions (
-    id SERIAL PRIMARY KEY ,
+CREATE TABLE t_permissions
+(
+    id   SERIAL PRIMARY KEY,
     role VARCHAR(255)
 );
 
-CREATE TABLE events (
-    id SERIAL PRIMARY KEY,
-    event_name VARCHAR(255),
-    event_date VARCHAR(255),
-    author VARCHAR(255),
+CREATE TABLE events
+(
+    id           SERIAL PRIMARY KEY,
+    event_name   VARCHAR(255),
+    event_date   VARCHAR(255),
+    author       VARCHAR(255),
     picture_link VARCHAR(255),
-    hall_id INT,
-    price INT,
-    status VARCHAR(255),
+    hall_id      INT,
+    price        INT,
+    status       VARCHAR(255),
     FOREIGN KEY (hall_id) REFERENCES halls (id)
 );
 
-CREATE TABLE t_users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255),
-    password VARCHAR(255),
+CREATE TABLE t_users
+(
+    id        SERIAL PRIMARY KEY,
+    email     VARCHAR(255),
+    password  VARCHAR(255),
     full_name VARCHAR(255),
-    img_link VARCHAR(255),
-    balance INT
+    img_link  VARCHAR(255),
+    balance   INT
 );
 
-CREATE TABLE comment (
-                         id SERIAL PRIMARY KEY,
-                         comment VARCHAR(255),
-                         event_id BIGINT,
-                         user_id BIGINT,
-                         FOREIGN KEY (event_id) REFERENCES events (id),
-                         FOREIGN KEY (user_id) REFERENCES t_users (id)
-);
-
-CREATE TABLE tickets (
-    id SERIAL PRIMARY KEY ,
+CREATE TABLE comment
+(
+    id       SERIAL PRIMARY KEY,
+    comment  VARCHAR(255),
     event_id BIGINT,
-    user_id BIGINT,
-    prime_code_ticket BIGINT,
-    place INT,
+    user_id  BIGINT,
     FOREIGN KEY (event_id) REFERENCES events (id),
     FOREIGN KEY (user_id) REFERENCES t_users (id)
 );
 
-CREATE TABLE t_users_permissions(
-                         userId BIGINT,
-                         permissionId BIGINT,
-                         FOREIGN KEY (userId) REFERENCES t_users (id),
-                         FOREIGN KEY (permissionId) REFERENCES t_permissions (id)
-  );
+CREATE TABLE tickets
+(
+    id                SERIAL PRIMARY KEY,
+    event_id          BIGINT,
+    user_id           BIGINT,
+    prime_code_ticket BIGINT,
+    place             INT,
+    FOREIGN KEY (event_id) REFERENCES events (id),
+    FOREIGN KEY (user_id) REFERENCES t_users (id)
+);
+
+CREATE TABLE t_users_permissions
+(
+    userId       BIGINT,
+    permissionId BIGINT,
+    FOREIGN KEY (userId) REFERENCES t_users (id),
+    FOREIGN KEY (permissionId) REFERENCES t_permissions (id)
+);
--- src/main/resources/db/changelog/changes/insert_init_data.sql
+++ src/main/resources/db/changelog/changes/insert_init_data.sql
@@ -1,20 +1,25 @@
 -- Вставка данных в таблицу t_permission
-INSERT INTO halls (place) VALUES
-                              (30),
-                              (30);
-INSERT INTO t_permission (role) VALUES
-                                     ('ROLE_USER'),
-                                     ('ROLE_ADMIN');
-INSERT INTO t_users (balance, email, full_name, img_link, password) VALUES
-   (2500, 'beksultan004@bk.ru', 'Bexultan Kuralbay', 'https://yt3.ggpht.com/a/AGF-l78RCnBXwItPz7NOMEmGVDdZ6Qaoss63865a8Q=s900-c-k-c0xffffffff-no-rj-mo', '$2a$10$AsRbNptKvBm3O/CataL/e.Q3BfmG21ftyk57GvzHMz/O5asbgc8I6'),
-   (0, 'omyrzakovbeksultan1@gmail.com', 'beks beks', 'https://yt3.ggpht.com/a/AGF-l78RCnBXwItPz7NOMEmGVDdZ6Qaoss63865a8Q=s900-c-k-c0xffffffff-no-rj-mo', '$2a$10$s/NlhDEwWVArROg1.9YOLu/B2pp3EUkyhMaH9S3C/OzHUzplwhsMO');
+INSERT INTO halls (place)
+VALUES (30),
+       (30);
+INSERT INTO t_permission (role)
+VALUES ('ROLE_USER'),
+       ('ROLE_ADMIN');
+INSERT INTO t_users (balance, email, full_name, img_link, password)
+VALUES (2500, 'beksultan004@bk.ru', 'Bexultan Kuralbay',
+        'https://yt3.ggpht.com/a/AGF-l78RCnBXwItPz7NOMEmGVDdZ6Qaoss63865a8Q=s900-c-k-c0xffffffff-no-rj-mo',
+        '$2a$10$AsRbNptKvBm3O/CataL/e.Q3BfmG21ftyk57GvzHMz/O5asbgc8I6'),
+       (0, 'omyrzakovbeksultan1@gmail.com', 'beks beks',
+        'https://yt3.ggpht.com/a/AGF-l78RCnBXwItPz7NOMEmGVDdZ6Qaoss63865a8Q=s900-c-k-c0xffffffff-no-rj-mo',
+        '$2a$10$s/NlhDEwWVArROg1.9YOLu/B2pp3EUkyhMaH9S3C/OzHUzplwhsMO');
 
-INSERT INTO t_users_permissions(user_id,permissions_id) VALUES
-    (1, 2),
-    (1, 1),
-    (2, 1);
-INSERT INTO events (event_name, author, event_date, picture_link, price, status, hall_id) VALUES
-    ('Meloman', 'Cinema', '2023-07-06', 'https://cdn.kino.kz/movies/Indiana_Jones_and_the_Dial_of_Destiny/p168x242.jpg', 2500, true, 2);
+INSERT INTO t_users_permissions(user_id, permissions_id)
+VALUES (1, 2),
+       (1, 1),
+       (2, 1);
+INSERT INTO events (event_name, author, event_date, picture_link, price, status, hall_id)
+VALUES ('Meloman', 'Cinema', '2023-07-06',
+        'https://cdn.kino.kz/movies/Indiana_Jones_and_the_Dial_of_Destiny/p168x242.jpg', 2500, true, 2);
 
 
 
